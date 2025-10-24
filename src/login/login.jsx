cda5d216ef8a4b8a75b2f346e9e33ba846be3842
Talmage Woodhouse <tw456@byu.edu>
2025-10-23 21:22:21 -0600

    login.jsx basic authState layout

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-export function Login() {
+import { AuthState } from "./authState";
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="d-flex flex-column justify-content-center align-items-center flex-grow-1 m-4">
       <div>
@@ -8,41 +10,49 @@ export function Login() {
 
         {authState === authState.Athenticated && (
           <div>
-            <h4 className="mb-4">This is where you take control of your life!</h4>
             <p className="lead">Welcome back, {userName}!</p>
-            <button className="btn btn-danger" onClick={() => onAuthChange(userName, AuthState.Unauthenticated)}>Logout</button>
+            <button
+              className="btn btn-danger"
+              onClick={() => onAuthChange(userName, AuthState.Unauthenticated)}
+            >
+              Logout
+            </button>
           </div>
-
         )}
         {AuthState === AuthState.Unauthenticated && (
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            const loginUserName = e.target.elements.username.value;
-            onAuthChange(loginUserName, AuthState.Authenticated);
-          }}
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const loginUserName = e.target.elements.username.value;
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          >
+            className="w-50"
+            <h4 className="mb-4 text-center">
+              This is where you take control of your life!
+            </h4>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="username/email"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="password"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary me-2">
+              Login
+            </button>
+            <button type="submit" className="btn btn-secondary">
+              Create
+            </button>
+          </form>
         )}
-      
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="username/email"
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary me-2">
-          Login
-        </button>
-        <button type="submit" className="btn btn-secondary">
-          Create
-        </button>
-      </form>
       </div>
     </main>
   );
