bfcc9db21b6f0b0065d44f6eeb95a48724c666fc
Talmage Woodhouse <tw456@byu.edu>
2025-11-11 15:56:12 -0700

    db habit edit

--- service/database.js
+++ service/database.js
@@ -43,11 +43,6 @@ async function updateUser(user) {
 
 // ------ HABIT FUNCTIONS ------
 
-// get all habits for a specific user
-async function getHabits(userEmail) {
-  return habitsCollection.find({ userEmail }).toArray();
-}
-
 // add a new habit
 async function addHabit(habit) {
   await habitsCollection.insertOne(habit);
